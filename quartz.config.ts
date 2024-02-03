44d366638227f155dad95411b472bc161c74192b
Yash Thakur <45539777+ysthakur@users.noreply.github.com>
2024-02-03 01:03:48 -0500

    Set baseUrl properly

--- quartz.config.ts
+++ quartz.config.ts
@@ -6,7 +6,7 @@ const config: QuartzConfig = {
     pageTitle: "CMSC320 Notes",
     enableSPA: true,
     enablePopovers: true,
-    baseUrl: "ysthakur.github.io/CMSC320-Notes",
+    baseUrl: "ysthakur.github.io/CMSC417-Notes/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
