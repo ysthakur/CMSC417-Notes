caf4bef9a072e1473318b57522d8cbffda40bfb6
Yash Thakur <45539777+ysthakur@users.noreply.github.com>
2024-10-09 15:57:59 -0400

    Add afterBody to shared page components

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,6 +6,7 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   footer: Component.Footer({}),
+  afterBody: [],
 }
 
 // components for pages that display a single page (e.g. a single note)
